002e5054efb9533259929304212f429783992922
Fahad <127460043+fahadfakrul@users.noreply.github.com>
2024-04-30 13:11:33 +0600

    put req added sucessfully

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const cors = require("cors");
-require('dotenv').config()
+require("dotenv").config();
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -10,13 +10,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-
-
-
-
-const uri =`mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.cnltwph.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
-
-
+const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.cnltwph.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -32,26 +26,49 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
-    const spotsCollection = client.db('spotsDB').collection('spots');
+    const spotsCollection = client.db("spotsDB").collection("spots");
 
-    app.get('/spots', async (req, res) => {
-        const cursor = spotsCollection.find();
-        const result = await cursor.toArray();
-        res.send(result);
-    })
+    app.get("/spots", async (req, res) => {
+      const cursor = spotsCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
 
-     app.post('/spots',async (req, res) =>{
-        const newSpots = req.body; 
-        // console.log(newSpots);
-        const result = await spotsCollection.insertOne(newSpots);
-        res.send(result);
-     })
+    app.post("/spots", async (req, res) => {
+      const newSpots = req.body;
+      // console.log(newSpots);
+      const result = await spotsCollection.insertOne(newSpots);
+      res.send(result);
+    });
 
-     app.get('/mySpots/:email', async (req , res) => {
-        console.log(req.params.email);
-        const result = await spotsCollection.find({email:req.params.email}).toArray();
-        res.send(result);
-     })
+    app.put("/updatespots/:id", async (req, res) => {
+      console.log(req.params.id);
+      const query = { _id: new ObjectId(req.params.id) };
+      const data = {
+        $set: {
+          tourists_spot_name: req.body.tourists_spot_name,
+          country_name: req.body.country_name,
+          location: req.body.location,
+          short_description: req.body.short_description,
+          avg_cost: req.body.avg_cost,
+          seasonality: req.body.seasonality,
+          travel_time: req.body.travel_time,
+          visitors_per_year: req.body.visitors_per_year,
+          photo: req.body.photo,
+        }
+      }
+    const result = await spotsCollection.updateOne(query, data);
+    console.log(result);
+    res.send(result);
+    });
+
+    app.get("/mySpots/:email", async (req, res) => {
+      console.log(req.params.email);
+      const result = await spotsCollection
+        .find({ email: req.params.email })
+        .toArray();
+      res.send(result);
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
